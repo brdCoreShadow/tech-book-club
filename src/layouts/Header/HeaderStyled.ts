6dc63af6794b487bf2a9763a2b7b9b549dcfc19b
Oleksandr-Berd <alex.berd86@gmail.com>
2025-08-03 08:38:05 +0300

    hero tablet is made

--- src/layouts/Header/HeaderStyled.ts
+++ src/layouts/Header/HeaderStyled.ts
@@ -4,4 +4,10 @@ export const HeaderCon = styled.header`
 padding-top: 24px;
 padding-bottom: 48px;
 padding-left: 16px;
+
+@media (min-width: 768px) and (max-width: 1279px) {
+  padding-top: 32px;
+  padding-bottom: 64px;
+  padding-left: 32px;
+}
 `
